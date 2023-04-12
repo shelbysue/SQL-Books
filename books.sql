771f9000f03e852c660ddc1ca216013b8c8f1c9b
shelbysue <101065139+shelbysue@users.noreply.github.com>
2023-04-12 18:36:09 -0400

    Add files via upload

---
+++ books.sql
@@ -0,0 +1,34 @@
+select * from Book
+
+select * from Book order by title;
+
+select * Book title sort by asc;
+
+select * from member
+where card_number = '77293110';
+
+delete from member
+where id = 2
+
+insert into Book (title, author)
+values ('moby dick', 'herman')
+
+insert into Book (title, author, checked_out_by_id)
+values ('don', 'miguel', 3)
+
+insert into Book (title, author, checked_out_by_id)
+values ('ulysses', 'james', 1)
+
+insert into Book (title, author, checked_out_by_id)
+values ('lost time', 'marcel post', 1)
+
+select * from member
+
+insert into member (name, card_number)
+values ('carter', 28209382)
+
+insert into member (name, card_number)
+values ('boris', 00000000)
+
+insert into member (name, card_number)
+values ('annabelle', 77293110)
